401f26ab06ebca5ee06ace79ac69d2cddb0299b4
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-04-06 01:56:45 -0300

    Update main.go

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 	var check = flag.String("c", "", "Check hashsum file.")
 	var target = flag.String("t", "", "Target file/wildcard to generate hashsum list.")
+	var recursive = flag.Bool("r", false, "Process directories recursively.")
 	var verbose = flag.Bool("v", false, "Verbose mode. (for CHECK command)")
 
 func main() {
@@ -23,13 +24,13 @@ func main() {
         if (len(os.Args) < 2) {
 	fmt.Println("GOST R 34.11-94 CryptoPro Hashsum Tool - ALBANESE Lab (c) 2020-2021\n")
 	fmt.Println("Usage of",os.Args[0]+":")
-        fmt.Printf("%s [-v] [-c <hash.g94>] -t <file.ext>\n\n", os.Args[0])
+        fmt.Printf("%s [-v] [-c <hash.g94>] [-r] -t <file.ext>\n\n", os.Args[0])
         flag.PrintDefaults()
         os.Exit(1)
         }
 
 
-        if *target != "" {
+        if *target != "" && *recursive == false {
 	files, err := filepath.Glob(*target)
 	if err != nil {
 	    log.Fatal(err)
@@ -46,6 +47,32 @@ func main() {
         }
     	fmt.Println(hex.EncodeToString(h.Sum(nil)), "*" + f.Name())
 	}
+	}
+
+        if *target != "" && *recursive == true {
+		err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			file, err := os.Stat(path)
+			if file.IsDir() {
+			} else {
+			h := gost341194.New(&gost28147.SboxIdGostR341194CryptoProParamSet)
+			f, err := os.Open(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if _, err := io.Copy(h, f); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(hex.EncodeToString(h.Sum(nil)), "*" + f.Name())
+			}
+			return nil
+		})
+		if err != nil {
+			log.Println(err)
+		}
 	}
 
 
