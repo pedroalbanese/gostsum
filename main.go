928678630a7a5b9a609e651b7625ec0220339d66
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-04-06 17:58:05 -0300

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	var check = flag.String("c", "", "Check hashsum file.")
 	var recursive = flag.Bool("r", false, "Process directories recursively.")
 	var target = flag.String("t", "", "Target file/wildcard to generate hashsum list.")
-	var verbose = flag.Bool("v", false, "Verbose mode. (for CHECK command)")
+	var verbose = flag.Bool("v", false, "Verbose mode. (The exit code is always 0 in this mode)")
 
 func main() {
     flag.Parse()
