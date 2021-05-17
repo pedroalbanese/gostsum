308cd7a942797b5e587d90eb8fd84fb6911819f5
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-05-17 09:18:27 -0300

    Update main.go

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ import (
 )
 
 	var check = flag.String("c", "", "Check hashsum file.")
-	var recursive = flag.Bool("r", false, "Process directories recursively.")
 	var target = flag.String("t", "", "Target file/wildcard to generate hashsum list.")
-	var verbose = flag.Bool("v", false, "Verbose mode. (The exit code is always 0 in this mode)")
+	var verbose = flag.Bool("v", false, "Verbose mode. (for CHECK command)")
+	var recursive = flag.Bool("r", false, "Process directories recursively.")
 
 func main() {
     flag.Parse()
@@ -24,7 +24,7 @@ func main() {
         if (len(os.Args) < 2) {
 	fmt.Println("GOST R 34.11-94 CryptoPro Hashsum Tool - ALBANESE Lab (c) 2020-2021\n")
 	fmt.Println("Usage of",os.Args[0]+":")
-        fmt.Printf("%s [-v] [-c <hash.g94>] [-r] -t <file.ext>\n\n", os.Args[0])
+        fmt.Printf("%s [-v] [-c <hash.g94>] -t <file.ext>\n\n", os.Args[0])
         flag.PrintDefaults()
         os.Exit(1)
         }
@@ -35,21 +35,24 @@ func main() {
 	if err != nil {
 	    log.Fatal(err)
 	}
-
 	for _, match := range files {
 	h := gost341194.New(&gost28147.SboxIdGostR341194CryptoProParamSet)
         f, err := os.Open(match)
         if err != nil {
             log.Fatal(err)
         }
+	file, err := os.Stat(match)
+	if file.IsDir() {
+	} else {
         if _, err := io.Copy(h, f); err != nil {
             log.Fatal(err)
         }
     	fmt.Println(hex.EncodeToString(h.Sum(nil)), "*" + f.Name())
 	}
 	}
+	}
 
-        if *target != "" && *recursive == true {
+	if *target != "" && *recursive == true {
 		err := filepath.Walk(filepath.Dir(*target),
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
@@ -83,32 +86,22 @@ func main() {
 		}
 	}
 
-
         if *check != "" {
 	file, err := os.Open(*check)
- 
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
 	}
- 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	var txtlines []string
- 
 	for scanner.Scan() {
 		txtlines = append(txtlines, scanner.Text())
 	}
- 
 	file.Close()
- 
-
 	for _, eachline := range txtlines {
 	lines := strings.Split(string(eachline), " *")
-
 	if strings.Contains(string(eachline), " *") {
-
 		h := gost341194.New(&gost28147.SboxIdGostR341194CryptoProParamSet)
-
 		_, err := os.Stat(lines[1])
 		if err == nil {
 			f, err := os.Open(lines[1])
@@ -138,5 +131,6 @@ func main() {
 		}
 		}
 		}
+
 	}
 }
